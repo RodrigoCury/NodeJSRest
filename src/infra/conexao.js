5c8cc50a622c5aac9cbf2262d6b8e676a89b2275
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-13 15:49:22 -0300

    Fix DB Name

--- src/infra/conexao.js
+++ src/infra/conexao.js
@@ -5,7 +5,7 @@ const conexao = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '30121996',
-    database: 'agenda-petshop',
+    database: 'agenda_petshop',
 })
 
 module.exports = conexao
