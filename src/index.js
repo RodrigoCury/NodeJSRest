28d8761dbee3543330dfcdf5e3487134bdf5eb5c
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-13 13:04:46 -0300

    added table Class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infra/conexao')
+const Tabelas = require('./infra/tabelas')
 
 conexao.connect((error) => {
     if (error) {
@@ -7,6 +8,7 @@ conexao.connect((error) => {
     } else {
         const app = customExpress()
 
+        Tabelas.init(conexao)
         app.listen(3000, () => console.log("Server port 3000"))
     }
 })
