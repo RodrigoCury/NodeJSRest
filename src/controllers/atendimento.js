c88fda07953e888ca48257747015928568549054
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-13 17:26:18 -0300

    Added Get from id| Patch | Delete

--- src/controllers/atendimento.js
+++ src/controllers/atendimento.js
@@ -2,11 +2,30 @@ const Atendimento = require('../models/Atendimento')
 
 module.exports = app => {
     app.get('/atendimento', (req, res) => {
-        res.send("Atendimento GET")
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimento/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorID(id, res)
     })
 
     app.post('/atendimento', (req, res) => {
         const atendimento = req.body
         Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch('/atendimento/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimento/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        console.log(id);
+
+        Atendimento.deletar(id, res)
     })
 }
