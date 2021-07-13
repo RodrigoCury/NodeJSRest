b7f005c97b326e425d022e50084c121bd580f975
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-13 17:26:48 -0300

    added listAll, SearchByID, Patch, Delete

--- src/models/Atendimento.js
+++ src/models/Atendimento.js
@@ -48,10 +48,98 @@ class Atendimento {
                     res.status(400).json(error)
                 } else {
                     res.status(201)
-                        .json(resultados)
+                        .json(atendimento)
                 }
             })
         }
+    }
+
+    lista(res) {
+        const sqlQuery = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sqlQuery, (error, resultados) => {
+            if (error) {
+                res.status(400).json(error)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorID(id, res) {
+        const sqlQuery = `SELECT * FROM Atendimentos WHERE id=${id}`
+
+        conexao.query(sqlQuery, (error, resultados) => {
+            if (error) {
+                res.status(400).json(error)
+            } else {
+                if (resultados.length) {
+                    res.status(200).json(resultados)
+                } else {
+                    res.status(404).json(resultados)
+                }
+            }
+        })
+    }
+
+    altera(id, valores, res) {
+        // Lista de erros 
+        const erros = []
+
+        // Tratamento de data caso mudou
+        if (valores.data) {
+            const data = moment(valores.data, "DD/MM/YYYY").format('YYYY-MM-DD[T]HH:mm:ss')
+            const dataEhValida = moment(data).isSameOrAfter(new Date())
+            if (dataEhValida) {
+                valores.data = data
+            } else {
+                erros.push({
+                    nome: "Data",
+                    mensagem: "Data precisa ser posterior ao momento de alteração"
+                })
+            }
+        }
+
+        // Se Nome mudou Checar por erros
+        if (valores.cliente) {
+            const clienteEhValido = valores.cliente.length >= 5
+            if (!clienteEhValido) {
+                erros.push({
+                    nome: "Cliente",
+                    mensagem: "Cliente precisa ter mais que 5 letras"
+                })
+            }
+        }
+
+
+        // Error First
+        if (erros.length) {
+            res.status(400).json(erros)
+        } else {
+            const sqlQuery = `UPDATE Atendimentos SET ? WHERE id=?`
+
+            conexao.query(sqlQuery, [valores, id], (error, resultados) => {
+                if (error) {
+                    res.status(400).json(error)
+                } else {
+                    res.status(200).json({ id, ...valores })
+                }
+            })
+        }
+
+    }
+
+    deletar(id, res) {
+        const sqlQuery = "DELETE FROM Atendimentos WHERE id=?"
+
+        conexao.query(sqlQuery, id, (error, resultados) => {
+            if (error) {
+                console.log(error)
+                res.status(400).json(error)
+            } else {
+                res.status(200).json({ id })
+            }
+        })
     }
 }
 
